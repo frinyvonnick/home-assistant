4adaba95920b04527a2b5d0b9b183d7845f9990c
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-10-07 14:16:31 +0200

    :wrench: Set up electron-vue boilerplate (#1)

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <h1>Hello world!</h1>  
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    methods: {
+      open(link) {
+        this.$electron.shell.openExternal(link)
+      },
+    },
+  }
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+
+  main {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    background-color: black;
+  }
+
+  h1 {
+    color: white;
+  }
+</style>
