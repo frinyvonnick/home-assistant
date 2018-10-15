141f4c8d1365e5540d865d15eb534451b102b4f3
Yoann Prot <yoann.prot@gmail.com>
2018-10-15 10:19:09 +0200

    :ok_hank: fix vue-carousel import

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
-import VueCarousel from 'vue-carousel'
 
 import App from './App'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
-
-Vue.use(VueCarousel)
 
 /* eslint-disable no-new */
 new Vue({
